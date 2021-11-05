6d5869c98f79ef439f2b091fc70bb6c0f7ffaa1e
philipvargas <91575086+philipvargas@users.noreply.github.com>
2021-11-04 19:59:14 -0500

    Update borrarclientes.js

--- js/borrarclientes.js
+++ js/borrarclientes.js
@@ -30,7 +30,7 @@ function borrarRegistro(llaveRegistro) {
 
     $.ajax({
         // la URL para la petición (url: "url al recurso o endpoint")
-        url: "http://localhost:8081/api/Client/" + llaveRegistro,
+        url: "http://129.151.125.65:8081/api/Client/" + llaveRegistro,
 
         // la información a enviar
         // (también es posible utilizar una cadena de datos)
