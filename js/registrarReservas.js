30359839d2320c7c560dbba8efb47a72a5d7c1cd
philipvargas <91575086+philipvargas@users.noreply.github.com>
2021-11-04 20:14:25 -0500

    Update registrarReservas.js

--- js/registrarReservas.js
+++ js/registrarReservas.js
@@ -17,7 +17,7 @@ function registrar() {
         $.ajax({
             // la URL para la petición (url: "url al recurso o endpoint")
             //url: "http://localhost:8081/api/Reservation/save",
-            url: "http://localhost:8081/api/Reservation/save",
+            url: "http://129.151.125.65:8081/api/Reservation/save",
 
             // la información a enviar
             // (también es posible utilizar una cadena de datos)
