6194a084edefce5612d04dd3b09aa5cf12f31f46
philipvargas <91575086+philipvargas@users.noreply.github.com>
2021-11-04 20:14:57 -0500

    Update registrarcategoria.js

--- js/registrarcategoria.js
+++ js/registrarcategoria.js
@@ -14,7 +14,7 @@ function registrar() {
     if (validar()){
         $.ajax({
             // la URL para la petición (url: "url al recurso o endpoint")
-            url: "http://localhost:8081/api/Category/save",
+            url: "http://129.151.125.65:8081/api/Category/save",
             
             // la información a enviar
             // (también es posible utilizar una cadena de datos)
