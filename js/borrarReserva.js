50d316c04c5e9edaac42541ff5cad5efde97ac1e
philipvargas <91575086+philipvargas@users.noreply.github.com>
2021-11-04 19:58:20 -0500

    Update borrarReserva.js

--- js/borrarReserva.js
+++ js/borrarReserva.js
@@ -26,7 +26,7 @@ function borrarRegistro(llaveRegistro) {
 
     $.ajax({
         // la URL para la petición (url: "url al recurso o endpoint")
-        url: "http://localhost:8081/api/Reservation/" + llaveRegistro,
+        url: "http://129.151.125.65:8081/api/Reservation/" + llaveRegistro,
 
         // la información a enviar
         // (también es posible utilizar una cadena de datos)
