2958720808bf3530f66ce7e0d6e3f8a39217d43a
philipvargas <91575086+philipvargas@users.noreply.github.com>
2021-11-04 20:15:54 -0500

    Update registrarmensajes.js

--- js/registrarmensajes.js
+++ js/registrarmensajes.js
@@ -15,7 +15,7 @@ function registrar() {
     if (validar()){
         $.ajax({
             // la URL para la petición (url: "url al recurso o endpoint")
-            url: "http://localhost:8081/api/Message/save",
+            url: "http://129.151.125.65:8081/api/Message/save",
             //url: "http://localhost:8081/api/Message/save",
             // la información a enviar
             // (también es posible utilizar una cadena de datos)
